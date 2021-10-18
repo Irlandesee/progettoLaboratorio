8a7d7020714c23ae2d63937c86e4e2dca26a6a18
Mattia Lunardi <mattia.lunardi@gmail.com>
2021-10-18 17:20:52 +0200

    almost completed module, read desc
    manca da lavorare sui bottoni, metodi check di classe?

--- src/gui/sanitari/RegistrazioneCentroVaccinaleWindow.java
+++ src/gui/sanitari/RegistrazioneCentroVaccinaleWindow.java
@@ -7,6 +7,10 @@ import java.awt.event.*;
 
 public class RegistrazioneCentroVaccinaleWindow extends JFrame{
 
+    /**
+     * Indirizzo: via/viale/piazza, nome, numero_civico, comune, sigla_provincia, cap
+     */
+
     private Container cont;
     private GridBagLayout grdbagLayout;
     private GridBagConstraints grdConstraints;
@@ -17,16 +21,29 @@ public class RegistrazioneCentroVaccinaleWindow extends JFrame{
     private JLabel lbl_nome_centro_vaccinale;
     private JLabel lbl_tipologia;
     private JLabel lbl_indirizzo_centro;
+    private JLabel lbl_nome_via;
+    private JLabel lbl_numero_civico;
+    private JLabel lbl_comune;
+    private JLabel lbl_sigla_provincia;
+    private JLabel lbl_cap;
 
     private JTextField txt_nome_centro_vaccinale;
-    private JTextField txt_tipologia;
-    private JTextField txt_indirizzo;
+    private JTextField txt_nome_via;
+    private JTextField txt_numero_civico; // only numbers!
+    private JTextField txt_comune;
+    private JTextField txt_sigla_provincia;
+    private JTextField txt_cap; // only numbers!!
+
+    private JComboBox combo_tipologia;
+    private JComboBox combo_qualificatoriVia;
+    private String[] tipiCentro = {"hub", "ospedale", "azienda"};
+    private String[] qualificatoriVia = {"via", "viale", "piazza"};
 
     public RegistrazioneCentroVaccinaleWindow(){
         super("Registrazione centro vaccinale");
         createInterface();
 
-        this.setSize(500, 400);
+        this.setSize(720, 400);
         this.setVisible(true);
 
     }
@@ -39,31 +56,56 @@ public class RegistrazioneCentroVaccinaleWindow extends JFrame{
         grdConstraints = new GridBagConstraints();
         grdConstraints.fill = GridBagConstraints.BOTH;
 
-        btn_add = new JButton("Aggiungi centro vaccinale");
+        btn_add = new JButton("Aggiungi");
         btn_reset = new JButton("Reset");
-
 
         lbl_nome_centro_vaccinale = new JLabel("Nome centro vaccinale");
         lbl_tipologia = new JLabel("tipologia");
-        lbl_indirizzo_centro = new JLabel("indirizzo centro");
+        lbl_indirizzo_centro = new JLabel("Via/Viale/Piazza");
+        lbl_comune = new JLabel("Comune");
+        lbl_nome_via = new JLabel("Nome via");
+        lbl_numero_civico = new JLabel("Numero civico");
+        lbl_sigla_provincia = new JLabel("Sigla provincia");
+        lbl_cap = new JLabel("cap");
 
         txt_nome_centro_vaccinale = new JTextField(20);
-        txt_tipologia = new JTextField(20);
-        txt_indirizzo = new JTextField(20);
+        txt_nome_via = new JTextField(20);
+        txt_numero_civico = new JTextField(5);
+        txt_comune = new JTextField(20);
+        txt_sigla_provincia = new JTextField();
+        txt_cap = new JTextField();
+        combo_tipologia = new JComboBox(tipiCentro);
+        combo_qualificatoriVia = new JComboBox(qualificatoriVia);
 
         addComponent(lbl_nome_centro_vaccinale, 0, 0, 1, 1);
-        addComponent(lbl_tipologia, 1, 0, 1, 1);
-        addComponent(lbl_indirizzo_centro, 2, 0, 1, 1);
         addComponent(txt_nome_centro_vaccinale, 0, 1, 20, 1);
-        addComponent(txt_tipologia, 1, 1, 20, 1);
-        addComponent(txt_indirizzo, 2, 1, 20, 1);
-        addComponent(btn_add, 3, 0, 1, 1);
-        addComponent(btn_reset, 3, 1, 1, 1);
+        addComponent(lbl_tipologia, 1, 0, 1, 1);
+        addComponent(combo_tipologia,1, 1, 1, 1);
+        addComponent(lbl_indirizzo_centro, 1, 2, 1, 1);
+        addComponent(combo_qualificatoriVia, 1, 3, 1, 1);
+        addComponent(lbl_nome_via, 2, 0, 1, 1);
+        addComponent(txt_nome_via, 2, 1, 20, 1);
+        addComponent(lbl_comune, 3, 0, 1,1);
+        addComponent(txt_comune, 3, 1, 20, 1);
+        addComponent(lbl_numero_civico, 4, 0, 1, 1);
+        addComponent(txt_numero_civico,4, 1, 1, 1);
+        addComponent(lbl_sigla_provincia, 4, 2, 1, 1);
+        addComponent(txt_sigla_provincia, 4, 3, 1, 1);
+        addComponent(lbl_cap, 5, 0, 1, 1);
+        addComponent(txt_cap, 5, 1, 1, 1);
+        addComponent(btn_add, 5, 2, 1, 1);
+        addComponent(btn_reset, 5, 3, 1, 1);
 
         ButtonHandler bHandler = new ButtonHandler();
         btn_add.addActionListener(bHandler);
         btn_reset.addActionListener(bHandler);
 
+        TextFieldHandler txtHandler = new TextFieldHandler();
+        txt_cap.addActionListener(txtHandler);
+        txt_numero_civico.addActionListener(txtHandler);
+        txt_sigla_provincia.addActionListener(txtHandler);
+        txt_comune.addActionListener(txtHandler);
+        txt_nome_via.addActionListener(txtHandler);
     }
 
     private void addComponent(Component c, int row, int column, int width, int height){
@@ -89,10 +131,72 @@ public class RegistrazioneCentroVaccinaleWindow extends JFrame{
 
     }
 
-    private class TextfieldHandler implements ActionListener{
+    private class TextFieldHandler implements ActionListener{
+
+        private final String newLine = "\n";
 
         public void actionPerformed(ActionEvent e){
+            if(e.getSource().equals(txt_numero_civico)){
+                String text = txt_numero_civico.getText();
+                if(checkForChars(text)){//ci sono lettere
+                    txt_numero_civico.setText("");
+                    JOptionPane.showMessageDialog(null, "Char warning", "Char Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+            else if(e.getSource().equals(txt_cap)){
+                String text = txt_cap.getText();
+                if(checkForChars(text)){
+                    txt_cap.setText("");
+                    JOptionPane.showMessageDialog(null, "Char warning", "Char Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+            else if(e.getSource().equals(txt_comune)){
+                String text = txt_comune.getText();
+                if(checkForDigits(text)){//ci sono numeri
+                    txt_comune.setText("");
+                    JOptionPane.showMessageDialog(null, "Digits warning", "Digits Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+            else if(e.getSource().equals(txt_nome_via)){
+                String text = txt_nome_via.getText();
+                if(checkForDigits(text)){
+                    txt_nome_via.setText("");
+                    JOptionPane.showMessageDialog(null, "Digits warning", "Digits Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+            else if(e.getSource().equals(txt_sigla_provincia)){
+                String text = txt_sigla_provincia.getText();
+                if(checkForDigits(text)){
+                    txt_sigla_provincia.setText("");
+                    JOptionPane.showMessageDialog(null, "Digits warning", "Digits Warning", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        }
 
+        /**
+         * Metodo per controllare che non ci siano numeri
+         * @param text
+         * @return true se ce un numero nella stringa
+         */
+        private boolean checkForDigits(String text){
+            for(int i = 0; i < text.length(); i++){
+                if(Character.isDigit(text.charAt(i)))
+                    return true;
+            }
+            return false;
+        }
+
+        /**
+         * Metodo per controllare che non ci siano lettere
+         * @param text
+         * @return true se non ci sono lettere nella stringa
+         */
+        private boolean checkForChars(String text){
+            for(int i = 0; i < text.length(); i++){
+                if(!Character.isDigit(text.charAt(i)))
+                    return true;
+            }
+            return false;
         }
 
     }
